31dbd2551cafd958929f577adaf1c0121f90c635
Yaseen Mustapha <yaseenmust@gmail.com>
2023-03-31 20:53:50 -0500

    update carousel

--- app/ImageCarousel.tsx
+++ app/ImageCarousel.tsx
@@ -1,75 +1,67 @@
 "use client";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 const images = [
   {
-    src: '/nikkah.png',
-    alt: 'Nikkah',
-    caption: 'Portrait of Aruba and Yaseen at their Nikkah',
+    src: "/nikkah.png",
+    caption: "Nikkah Illustration",
   },
   {
-    src: '/image1.jpg',
-    alt: 'Image 1',
-    caption: 'Caption for image 1',
+    src: "/nikkah2.jpg",
+    caption: "Nikkah IRL",
   },
   {
-    src: '/image2.jpg',
-    alt: 'Image 2',
-    caption: 'Caption for image 2',
+    src: "/image1.jpg",
+    caption:
+      "Canyon Lights at Capilano Suspension Bridge Park",
   },
   {
-    src: '/image3.jpg',
-    alt: 'Image 3',
-    caption: 'Caption for image 3',
+    src: "/image2.jpg",
+    caption: "Water's Edge Café",
   },
   {
-    src: '/image4.jpg',
-    alt: 'Image 4',
-    caption: 'Caption for image 4',
+    src: "/image3.jpg",
+    caption: "Dessert at Happy Singh Street Eats",
   },
   {
-    src: '/image5.jpg',
-    alt: 'Image 5',
-    caption: 'Caption for image 5',
+    src: "/image4.jpg",
+    caption: "Valentine's Day 2023",
   },
   {
-    src: '/image6.jpg',
-    alt: 'Image 6',
-    caption: 'Caption for image 6',
+    src: "/image5.jpg",
+    caption: "Ski day with friends at Cypress Mountain",
   },
   {
-    src: '/image7.jpg',
-    alt: 'Image 7',
-    caption: 'Caption for image 7',
+    src: "/image6.jpg",
+    caption: "Hina's Dholki",
   },
   {
-    src: '/image8.jpg',
-    alt: 'Image 8',
-    caption: 'Caption for image 8',
+    src: "/image7.jpg",
+    caption: "Out for dessert",
   },
   {
-    src: '/image9.jpg',
-    alt: 'Image 9',
-    caption: 'Caption for image 9',
+    src: "/image8.jpg",
+    caption: "Polaroid from the Nikkah",
   },
   {
-    src: '/image10.jpg',
-    alt: 'Image 10',
-    caption: 'Caption for image 10',
+    src: "/image9.jpg",
+    caption: "Dinner at RH Rooftop Restaurant",
   },
   {
-    src: '/image11.jpg',
-    alt: 'Image 11',
-    caption: 'Caption for image 11',
+    src: "/image10.jpg",
+    caption: "With Yaseen's family at Capilano Suspension Bridge Park",
   },
   {
-    src: '/image12.jpg',
-    alt: 'Image 12',
-    caption: 'Caption for image 12',
+    src: "/image11.jpg",
+    caption: "Cooper's Hawk Restaurant",
+  },
+  {
+    src: "/image12.jpg",
+    caption: "The Bean at Millennium Park",
   },
 ];
 
@@ -78,10 +70,9 @@ export default function ImageCarousel() {
     <Carousel showArrows={true} autoPlay infiniteLoop swipeable dynamicHeight>
       {images.map((image, index) => (
         <div key={index}>
-          <img src={image.src} alt={image.alt} />
-          <p className="legend">{image.caption}</p>
+          <img src={image.src} alt={image.caption} />
         </div>
       ))}
     </Carousel>
   );
-};
+}
