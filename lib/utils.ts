cb9595b83a7e057ac9c1ce4d29ae1e4c1257042a
Yaseen Mustapha <yaseenmust@gmail.com>
2023-07-01 00:21:11 -0500

    fix: remove UploadThing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,3 @@
-import { generateComponents } from "@uploadthing/react";
-import type { OurFileRouter } from "@/app/api/uploadThing/core";
-
 export function formatDate(input: string | number): string {
   const date = new Date(input);
   return date.toLocaleTimeString("en-US", {
@@ -9,6 +6,3 @@ export function formatDate(input: string | number): string {
     hour12: true,
   });
 }
-
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
