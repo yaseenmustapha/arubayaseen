c61c28572757066885b3ffe805652addc1580727
Yaseen Mustapha <yaseenmust@gmail.com>
2023-07-01 02:32:36 -0500

    live chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,8 +1,26 @@
-export default function Chat() {
+import Feed from "./Feed";
+
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`, {
+    cache: "no-store",
+  });
+  return res.json();
+}
+
+export default async function Chat() {
+  // const data: {
+  //   id: string;
+  //   image: string;
+  //   content: string;
+  //   user: { id: string; name: string; image: string };
+  //   createdAt: string;
+  //   likes: [];
+  // }[] = await getPosts();
+
   return (
     <main>
-      <div className="max-w-2xl mx-auto -mt-36">
-        <p className="font-gilda text-center">Chat goes live on wedding day!</p>
+      <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
+        {/* <Feed posts={data} /> */}
       </div>
     </main>
   );
