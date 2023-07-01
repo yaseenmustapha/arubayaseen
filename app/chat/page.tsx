aedb01e20708e181096d9ecc684ee53fd35d32b9
Yaseen Mustapha <yaseenmust@gmail.com>
2023-07-01 00:29:15 -0500

    test

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -21,7 +21,7 @@ export default async function Chat() {
   return (
     <main>
       <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
-        <Feed posts={data} />
+        
       </div>
     </main>
   );
