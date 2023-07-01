8a932a105e5099b21bfd806d3f2632989c5707e8
Yaseen Mustapha <yaseenmust@gmail.com>
2023-07-01 16:37:42 -0500

    Add refresh button and update Enhance button style

--- app/chat/AddPost.tsx
+++ app/chat/AddPost.tsx
@@ -169,7 +169,7 @@ export default function AddPost(): JSX.Element {
         <button
           type="button"
           disabled={!content || enhancing}
-          className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded-md ml-2"
+          className="bg-gradient-to-r from-blue-400 to-gray-300 hover:from-pink-500 hover:to-yellow-500 text-white font-bold py-2 px-4 rounded-md ml-2"
           onClick={enhanceWithAI}
         >
           {enhancing ? "Enhancing..." : "Enhance with AI 😎"}
--- app/chat/Feed.tsx
+++ app/chat/Feed.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Post from "./Post";
 import AddPost from "./AddPost";
+import { useRouter } from "next/navigation";
 
 export default function Feed({
   posts,
@@ -18,14 +19,28 @@ export default function Feed({
 }) {
   const { data: session, status } = useSession();
   const { user } = session || {};
+  const router = useRouter();
+
+  const handleRefresh = () => {
+    router.refresh();
+  };
 
   return (
     <>
+      {session && (
+        <button
+          className="font-gilda text-center border border-gray-500 rounded-md px-4 py-2 block mx-auto -mt-12 mb-6"
+          onClick={handleRefresh}
+        >
+          Refresh Feed
+        </button>
+      )}
+
       {session ? (
         <AddPost />
       ) : (
         <button
-          className="font-gilda text-center border border-gray-500 rounded-md px-4 py-2 block mx-auto"
+          className="font-gilda text-center border border-blue-500 rounded-md px-4 py-2 block mx-auto"
           onClick={() => signIn()}
         >
           Login to use chat
